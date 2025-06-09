ed7c61b6ae0c83efabe17bbe213a40a8f2b07644
Jiří Strnadel <Juzba88@gmail.com>
2025-06-09 18:42:18 +0200

    Přidejte soubory projektu.

---
+++ JobPortal/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+using JobPortal.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace JobPortal.Controllers
+{
+    public class HomeController(ILogger<HomeController> logger) : Controller
+    {
+        private readonly ILogger<HomeController> _logger = logger;
+
+        public IActionResult Index() => View();
+
+        public async Task<IActionResult> JobList() => View();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
